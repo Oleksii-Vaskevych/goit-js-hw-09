d01a63a9ce5b87a44f4d3e215ddd9b6a515e01e4
Oleksii Vaskevych <alexeyffi@ukr.net>
2022-11-16 00:27:02 +0200

    task01-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const CHANGECOLOR_DELAY = 1000;
+let timerId = null;
 
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
+
+refs.buttonStart.addEventListener('click', onStartBtn);
+refs.buttonStop.addEventListener('click', onStopBtn);
+
+function onStartBtn(event) {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, CHANGECOLOR_DELAY);
+  refs.buttonStart.disabled = true;
+  refs.buttonStop.disabled = false;
+}
+
+function onStopBtn(event) {
+  clearInterval(timerId);
+  refs.buttonStop.disabled = true;
+  refs.buttonStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
