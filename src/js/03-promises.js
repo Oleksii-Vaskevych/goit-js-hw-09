6268dfcbd65634f8305305a8e71e22da21ef0b09
Oleksii Vaskevych <alexeyffi@ukr.net>
2022-11-20 19:44:36 +0200

    fix task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,29 +17,25 @@ function onFormSubmit(event) {
   );
 
   for (let i = 0; i < Number(amount.value); i++) {
-    createPromise(i + 1, Number(delay.value) + i * Number(step.value));
+    createPromise(i + 1, Number(delay.value) + i * Number(step.value))
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resolve(
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-        );
+        resolve({ position, delay });
       } else {
-        reject(Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`));
+        reject({ position, delay });
       }
     }, delay);
   });
 }
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
